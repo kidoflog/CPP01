69d121049cec79fd91c266b0192d6a63344ffa04
kkido <kkido@student.42tokyo.jp>
2026-05-05 23:31:02 +0900

    make Class

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkido <kkido@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/05 23:22:32 by kkido             #+#    #+#             */
+/*   Updated: 2026/05/05 23:30:19 by kkido            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(const std::string &new_name) : _name(new_name){
+}
+
+Zombie::~Zombie(){
+	std::cout << _name << "is destroyed..." << std::endl;
+}
+
+void Zombie::announce(){
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkido <kkido@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/05 23:11:12 by kkido             #+#    #+#             */
+/*   Updated: 2026/05/05 23:30:31 by kkido            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+	public:
+		void announce();
+		Zombie(const std::string &new_name);
+		~Zombie();
+	private:
+		std::string _name;
+};
+
+
+#endif
