efa6c50890214851e7935753930b302072e01dc8
Flog <kaoru.kido@fuji.waseda.jp>
2026-05-08 11:02:15 +0900

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkido <kkido@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/07 18:55:36 by kkido             #+#    #+#             */
+/*   Updated: 2026/05/08 11:00:48 by kkido            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+
+int main (){
+	std::string str = "HI THIS IS BRIAN";
+	std::string *ptr = &str;
+	std::string &ref = str;
+	std::cout << "str adress: " << &str << std::endl;
+	std::cout << "ptr adress: " << ptr << std::endl;
+	std::cout << "ref adress: " << &ref << std::endl;
+	std::cout << "str value: " << str << std::endl;
+	std::cout << "ptr value: " << *ptr << std::endl;
+	std::cout << "ref value: " << ref << std::endl;
+}
